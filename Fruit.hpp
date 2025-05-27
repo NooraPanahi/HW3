14d1b09c3a2fa1219f1c0d761ae41a197d6aeda2
Noora Panahi <nourap064@gmail.com>
2025-05-27 13:04:34 -0700

    Fruit Header

---
+++ Fruit.hpp
@@ -0,0 +1,25 @@
+#ifndef FRUIT_H
+#define FRUIT_H
+#include <string>
+#include <vector>
+#include "Item.hpp"
+
+class Fruit : public Item {
+    
+    int price;
+    int available;
+    std::string measurment ;
+    std::string name ; 
+
+    public:
+        Fruit( std::string name ,int p , int a ,std::string me = "kg");
+        void buy(int b) override; 
+        std::string getInfo() override ;
+        std::string get_Name() const override ;  
+        int getPrice() const override; 
+        int get_available()const override ; 
+        void set_available(int a) override; 
+        bool is_available() const override; 
+
+};
+#endif
