6cf618b66aff22aa2f15c1a57b89e15d4eb40eb7
Noora Panahi <nourap064@gmail.com>
2025-05-27 13:06:00 -0700

    item Header

---
+++ Item.hpp
@@ -0,0 +1,26 @@
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
+#include <iostream>
+
+class Item {
+    private : 
+
+        int price;
+        std::string name ; 
+        int masurment;
+        int available;
+
+    public:
+        Item(std::string , int ,int );
+        void operator-( int i);
+        bool virtual is_available() const = 0;
+        virtual void buy(int b) = 0 ;
+        virtual std::string getInfo() = 0;  
+        int virtual get_available() const = 0 ; 
+        int virtual getPrice() const = 0 ; 
+        std::string virtual get_Name() const =0 ;
+        void virtual set_available(int a) = 0 ;  
+
+};
+#endif
